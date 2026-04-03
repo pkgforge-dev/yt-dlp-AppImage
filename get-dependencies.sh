dc057ebd8d4637f2c1758cc68616b7a95a19d5b1
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-03 05:47:11 -0400

    get app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,11 +6,11 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm yt-dlp
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano ! mesa ! vulkan
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
