e9692f1eace5c3096d33ac8c1eed694ac78491c3
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-03 06:07:25 -0400

    simple test

--- make-appimage.sh
+++ make-appimage.sh
@@ -28,4 +28,4 @@ quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the test fails due to the app
 # having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
