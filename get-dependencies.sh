0781f2123855430263467196514def5247b44c44
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-05 12:21:45 -0400

    add bun

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm yt-dlp
+pacman -Syu --noconfirm yt-dlp bun
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
